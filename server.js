d2e86829687cb2e546004bf302cdfd2ca5c179b6
Teeemster <jonathan_beach@ymail.com>
2021-12-03 14:15:31 -0700

    connected server

--- server.js
+++ server.js
@@ -2,17 +2,26 @@
 const express = require('express');
 
 //Set up the server
-//const db = require('./db/db.json')
-const PORT = 3001;
+//Const db = require('./db/db.json')
+const PORT = process.env.PORT || 3001;
+
+//Require path
+const path = require('path')
 
 //Initialize the app
 const app = express();
 
 // GET the initial HTML
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+//Open Notes Page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//Flip server on and listen
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
